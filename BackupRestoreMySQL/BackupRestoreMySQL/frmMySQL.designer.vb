68699844c5a28888828250def88e561b5e85e454
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-09-28 12:13:29 +0700

    Add files via upload

---
+++ BackupRestoreMySQL/BackupRestoreMySQL/frmMySQL.designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMySQL
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMySQL))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtDBPassword = New System.Windows.Forms.TextBox()
+        Me.txtDBUserName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtServer = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnRestore = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnBackup = New System.Windows.Forms.Button()
+        Me.cmbDataBase = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.btnConnect)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txtDBPassword)
+        Me.GroupBox1.Controls.Add(Me.txtDBUserName)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtServer)
+        Me.GroupBox1.Location = New System.Drawing.Point(2, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(502, 161)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = " Authentication "
+        '
+        'btnConnect
+        '
+        Me.btnConnect.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.btnConnect.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnConnect.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnConnect.ForeColor = System.Drawing.Color.White
+        Me.btnConnect.Location = New System.Drawing.Point(219, 118)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(119, 34)
+        Me.btnConnect.TabIndex = 23
+        Me.btnConnect.Text = "Connect Server"
+        Me.btnConnect.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Black
+        Me.Label5.Location = New System.Drawing.Point(28, 87)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 19)
+        Me.Label5.TabIndex = 22
+        Me.Label5.Text = "Password"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Black
+        Me.Label6.Location = New System.Drawing.Point(28, 52)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(70, 19)
+        Me.Label6.TabIndex = 21
+        Me.Label6.Text = "User ID"
+        '
+        'txtDBPassword
+        '
+        Me.txtDBPassword.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtDBPassword.Location = New System.Drawing.Point(156, 84)
+        Me.txtDBPassword.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtDBPassword.Name = "txtDBPassword"
+        Me.txtDBPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtDBPassword.Size = New System.Drawing.Size(246, 27)
+        Me.txtDBPassword.TabIndex = 20
+        Me.txtDBPassword.Text = "admin"
+        Me.txtDBPassword.UseSystemPasswordChar = True
+        '
+        'txtDBUserName
+        '
+        Me.txtDBUserName.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtDBUserName.Location = New System.Drawing.Point(156, 49)
+        Me.txtDBUserName.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtDBUserName.Name = "txtDBUserName"
+        Me.txtDBUserName.Size = New System.Drawing.Size(246, 27)
+        Me.txtDBUserName.TabIndex = 19
+        Me.txtDBUserName.Text = "admin"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(28, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 18)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Server"
+        '
+        'txtServer
+        '
+        Me.txtServer.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtServer.Location = New System.Drawing.Point(156, 15)
+        Me.txtServer.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtServer.Name = "txtServer"
+        Me.txtServer.Size = New System.Drawing.Size(246, 27)
+        Me.txtServer.TabIndex = 17
+        Me.txtServer.Text = "localhost"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.btnExit)
+        Me.GroupBox2.Controls.Add(Me.btnRestore)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.btnBackup)
+        Me.GroupBox2.Controls.Add(Me.cmbDataBase)
+        Me.GroupBox2.Location = New System.Drawing.Point(2, 162)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(502, 119)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = " DataBase Informations "
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.LemonChiffon
+        Me.btnExit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnExit.FlatAppearance.BorderSize = 0
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Image = CType(resources.GetObject("btnExit.Image"), System.Drawing.Image)
+        Me.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnExit.Location = New System.Drawing.Point(6, 73)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(111, 33)
+        Me.btnExit.TabIndex = 16
+        Me.btnExit.Text = "E&xit Program"
+        Me.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnRestore
+        '
+        Me.btnRestore.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.btnRestore.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnRestore.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRestore.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnRestore.ForeColor = System.Drawing.Color.White
+        Me.btnRestore.Location = New System.Drawing.Point(283, 73)
+        Me.btnRestore.Name = "btnRestore"
+        Me.btnRestore.Size = New System.Drawing.Size(119, 34)
+        Me.btnRestore.TabIndex = 15
+        Me.btnRestore.Text = "Restore DtaBase"
+        Me.btnRestore.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label4.Location = New System.Drawing.Point(28, 35)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(79, 18)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "DataBase"
+        '
+        'btnBackup
+        '
+        Me.btnBackup.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.btnBackup.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnBackup.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnBackup.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnBackup.ForeColor = System.Drawing.Color.White
+        Me.btnBackup.Location = New System.Drawing.Point(154, 73)
+        Me.btnBackup.Name = "btnBackup"
+        Me.btnBackup.Size = New System.Drawing.Size(119, 34)
+        Me.btnBackup.TabIndex = 14
+        Me.btnBackup.Text = "Backup DtaBase"
+        Me.btnBackup.UseVisualStyleBackColor = False
+        '
+        'cmbDataBase
+        '
+        Me.cmbDataBase.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbDataBase.FormattingEnabled = True
+        Me.cmbDataBase.Location = New System.Drawing.Point(154, 34)
+        Me.cmbDataBase.Name = "cmbDataBase"
+        Me.cmbDataBase.Size = New System.Drawing.Size(248, 24)
+        Me.cmbDataBase.TabIndex = 0
+        '
+        'frmMySQL
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(507, 283)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMySQL"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Backup/Restore MySQL Server - Code bY: Thongkorn Tubtimkrob"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnConnect As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtDBPassword As System.Windows.Forms.TextBox
+    Friend WithEvents txtDBUserName As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtServer As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnRestore As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnBackup As System.Windows.Forms.Button
+    Friend WithEvents cmbDataBase As System.Windows.Forms.ComboBox
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ BackupRestoreMySQL/BackupRestoreMySQL/frmMySQL.vb
@@ -0,0 +1,211 @@
+﻿#Region "ABOUT"
+' / --------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gnet.com/webboard
+' /
+' / Purpose: Backup and Restore MySQL Server.
+' / Microsoft Visual Basic .NET (2010) + MySQL Server.
+' /
+' / This is open source code under @Copyleft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------
+#End Region
+
+Imports MySql.Data.MySqlClient
+Imports System.IO
+
+Public Class frmMySQL
+    '// Declare variable one time but use many times.
+    Dim Conn As MySqlConnection
+    Dim Cmd As MySqlCommand
+    Dim DR As MySqlDataReader
+    Dim DA As MySqlDataAdapter
+    Dim strSQL As String
+    '//
+    Dim MyPathBackup As String = GetPath(Application.StartupPath + "Backup")
+    ' / --------------------------------------------------------------------------------
+    '// MySQL Server DBMS Connection Test with VB.NET (2010).
+    Public Function ConnectMySQL(ByVal SERVER As String, ByVal UID As String, PWD As String, ByRef cmb As ComboBox, Optional ByVal DB As String = "") As Boolean
+        Dim strCon As String = String.Empty
+        '// Connect DataBase.
+        If DB = "" Then
+            strCon = _
+                " Server=" & SERVER & "; " & _
+                " User ID=" & UID & "; " & _
+                " Password=" & PWD & "; " & _
+                " Port=3306; " & _
+                " CharSet=utf8; " & _
+                " Connect Timeout=120000; " & _
+                " Pooling = True; " & _
+                " Persist Security Info=True; " & _
+                " Connection Reset=False; "
+        Else
+            strCon = _
+                " Server=" & SERVER & "; " & _
+                " User ID=" & UID & "; " & _
+                " Database=" & DB & "; " & _
+                " Password=" & PWD & "; " & _
+                " Port=3306; " & _
+                " CharSet=utf8; " & _
+                " Connect Timeout=120000; " & _
+                " Pooling = True; " & _
+                " Persist Security Info=True; " & _
+                " Connection Reset=False; "
+        End If
+        Try
+            Conn = New MySqlConnection
+            Conn.ConnectionString = strCon
+            '// Connect MySQL Server and Listing All DataBase.
+            If DB = "" Then
+                Conn.Open()
+                Cmd = New MySqlCommand("SHOW DATABASES", Conn)
+                DR = Cmd.ExecuteReader()
+                If DR.HasRows Then
+                    cmb.Items.Clear()
+                    While DR.Read()
+                        cmb.Items.Add(DR("Database").ToString)
+                    End While
+                    cmb.SelectedIndex = 0
+                End If
+                DR.Close()
+                Cmd.Dispose()
+                Conn.Close()
+                Conn.Dispose()
+                MessageBox.Show("Connect MySQL Server Complete.", "Report Status", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Return True
+
+                '//
+            Else
+                If Conn.State = ConnectionState.Open Then Conn.Close()
+                Conn.Dispose()
+                Return True
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Report Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return False
+        End Try
+    End Function
+
+    '// START HERE.
+    Private Sub frmMySQL_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Try
+            '// Create Backup Folder if doesn't exist.
+            If (Not System.IO.Directory.Exists(GetPath(Application.StartupPath) & "Backup")) Then System.IO.Directory.CreateDirectory(GetPath(Application.StartupPath) & "Backup")
+            btnConnect.Enabled = True
+            btnBackup.Enabled = False
+            btnRestore.Enabled = False
+            cmbDataBase.Enabled = False
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '// Connect to MySQL Server and List all Databases.
+    Private Sub btnConnect_Click(sender As System.Object, e As System.EventArgs) Handles btnConnect.Click
+        Try
+            '// Parameters: SERVER_NAME, DB_USERNAME, DB_PASSWORD, ComboBox Control Name, DB_NAME (Default = "")
+            If Not ConnectMySQL(txtServer.Text.Trim, txtDBUserName.Text.Trim, txtDBPassword.Text.Trim, cmbDataBase) Then Return
+            btnConnect.Enabled = False
+            btnBackup.Enabled = True
+            btnRestore.Enabled = True
+            cmbDataBase.Enabled = True
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '// Backup DataBase.
+    Private Sub btnBackup_Click(sender As System.Object, e As System.EventArgs) Handles btnBackup.Click
+        Dim DBFile As String = String.Empty
+        Dim dlgSaveFile = New SaveFileDialog
+        Try
+            With dlgSaveFile
+                .Title = "Save MySQL DataBase"
+                .InitialDirectory = MyPathBackup
+                .Filter = "SQL Dump File (*.sql)|*.sql"
+                '// Filename: DBName-ddMMyyyy-HHmmss 
+                '// Ex. dbfood-28092564-104812.sql
+                .FileName = cmbDataBase.Text + "-" + DateTime.Now.ToString("ddMMyyyy-HHmmss") + ".sql"
+                .RestoreDirectory = True
+            End With
+            If dlgSaveFile.ShowDialog = DialogResult.OK Then
+                '// Parameters: SERVER_NAME, DB_USERNAME, DB_PASSWORD, ComboBox Control Name, DB_NAME
+                If ConnectMySQL(txtServer.Text.Trim, txtDBUserName.Text.Trim, txtDBPassword.Text.Trim, cmbDataBase, cmbDataBase.Text) Then
+                    DBFile = dlgSaveFile.FileName
+                    Using Cmd = New MySqlCommand()
+                        Using mb As MySqlBackup = New MySqlBackup(Cmd)
+                            Cmd.Connection = Conn
+                            Conn.Open()
+                            mb.ExportToFile(DBFile)
+                            Conn.Close()
+                        End Using
+                    End Using
+                    MessageBox.Show("Backup Your MySQL DataBase Successfully!", "Backup MySQL DataBase", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    '// Restore DataBase.
+    Private Sub btnRestore_Click(sender As System.Object, e As System.EventArgs) Handles btnRestore.Click
+        Dim DBFile As String = String.Empty
+        Dim dlgOpenFile = New OpenFileDialog
+        Try
+            With dlgOpenFile
+                .Title = "Open MySQL DataBase"
+                .InitialDirectory = MyPathBackup
+                .Filter = "SQL Dump File (*.sql)|*.sql"
+                .RestoreDirectory = True
+            End With
+            If dlgOpenFile.ShowDialog = DialogResult.OK Then
+                Dim Result As Byte = MessageBox.Show("Are you sure to RESTORE into DataBase Name: " & cmbDataBase.Text & "?", "Confirm to Restore", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+                If Result = DialogResult.Yes Then
+                    '// Parameters: SERVER_NAME, DB_USERNAME, DB_PASSWORD, ComboBox Control, DB_NAME (Optional)
+                    If ConnectMySQL(txtServer.Text.Trim, txtDBUserName.Text.Trim, txtDBPassword.Text.Trim, cmbDataBase, cmbDataBase.Text) Then
+                        DBFile = dlgOpenFile.FileName
+                        Using Cmd = New MySqlCommand()
+                            Using mb As MySqlBackup = New MySqlBackup(Cmd)
+                                Cmd.Connection = Conn
+                                Conn.Open()
+                                mb.ImportFromFile(DBFile)
+                                Conn.Close()
+                            End Using
+                        End Using
+                        MessageBox.Show("Restore your MySQL DataBase Successfully!", "Restore MySQL DataBase", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    End If
+                End If
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    ' / Get my project path
+    Function GetPath(AppPath As String) As String
+        '/ MessageBox.Show(AppPath);
+        '/ Return Value
+        GetPath = AppPath.ToLower.Replace("\bin\debug", "\").Replace("\bin\release", "\").Replace("\bin\x86\debug", "\")
+        '// If not found folder then put the \ (BackSlash ASCII Code = 92) at the end.
+        If Microsoft.VisualBasic.Right(GetPath, 1) <> Chr(92) Then GetPath = GetPath & Chr(92)
+    End Function
+
+    Private Sub btnExit_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmMySQL_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+End Class
